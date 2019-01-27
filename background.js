bc8bd23c9a65e9c1025ea80f76d9aae99a2539a5
Kévin GRILLET <kevin@pc53.home>
2019-01-27 09:31:07 +0100

    Disabled + new tab

--- background.js
+++ background.js
@@ -2,10 +2,29 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
 	var parser = document.createElement('a');
 	parser.href = tab[0].url;
-	var regExp = /^.*(youtube\.com\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
-	if (tab[0].url.match(regExp)) {
+	if (tab[0].url.match(/^.*(youtube\.com\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)) {
 	  parser.hostname = "yout.com";	
-      chrome.tabs.update(tab.id, {url: parser.href});
+      chrome.tabs.create({url: parser.href});
     }
   });
+});
+
+var updateLogo = function(url, tabId){
+  if (url == undefined) {
+    chrome.browserAction.setIcon({path: 'logo-disabled.png', tabId: tabId});
+  } else if (url.match(/^.*(youtube\.com\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)){
+    chrome.browserAction.setIcon({path: 'logo.png', tabId: tabId});
+  } else {
+    chrome.browserAction.setIcon({path: 'logo-disabled.png', tabId: tabId});
+  }	
+}
+
+chrome.tabs.onActivated.addListener(function(info){
+  chrome.tabs.get(info.tabId, function(change){
+	updateLogo(change.url, info.tabId);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  updateLogo(tab.url, tabId);
 });
